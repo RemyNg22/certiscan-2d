3d1c1f5a0d4d77247905e5addb07d84adb2d19a3
RemyNg22 <remyng22@gmail.com>
2026-06-09 18:02:53 +0200

    Fonction extraction 2D code

--- certiscan2d/extractor.py
+++ certiscan2d/extractor.py
@@ -2,10 +2,21 @@ import pymupdf
 import cv2
 from pathlib import Path
 import numpy as np
-import zxingcpp, pyzbar
+import zxingcpp
 
-class DataMatrixNotFoundError(Exception):
+# création des différentes exceptions possibles
+class ExtractionError(Exception):
     pass
+
+class InvalidImageError(ExtractionError):
+    pass
+
+class DataMatrixNotFoundError(ExtractionError):
+    pass
+
+class MultipleDataMatrixFoundError(ExtractionError):
+    pass
+
 
 def normalisation_document(file_path : Path):
     file_path = Path(file_path)
@@ -49,8 +60,45 @@ def traitement_image(img):
             "binary" : binary}
 
 
-def extract_2d_code(img):
-    pass
+def extract_2d_code(gray : np.ndarray, binary: np.ndarray):
+    """
+    Extraction des 2D codes détectés dans une image, lève une exception 
+    si soucis de lecture d'image, si erreur zxing, si pas de 2D code ou si 
+    plus d'un 2D code sur le document
+    """
+
+    if gray is None or binary is None:
+        raise InvalidImageError("Image invalide (None)")
+    
+    def decode(img):
+        results = []
+        try:
+            decoded = zxingcpp.read_barcodes(img) #on essaie de trouver les qrcodes
+            for r in decoded:
+                if r.text:
+                    results.append(r.text.strip())
+        except Exception as exc:
+            raise RuntimeError(f"Erreur ZXing {exc}") from exc
+        return results
+    
+    codes = decode(binary) #on teste d'abord la fonction avec binary et si rien, avec gray
+
+    if not codes:
+        codes = decode(gray)
+
+    unique_codes = list(set(codes))
+
+    if len(unique_codes) > 1:
+        raise MultipleDataMatrixFoundError(f"Plusieurs codes détectés : {unique_codes}")
+    
+    return unique_codes[0]
 
 def extract_text(img):
+    pass
+
+
+def extract_page():
+    pass
+
+def extract_document():
     pass
